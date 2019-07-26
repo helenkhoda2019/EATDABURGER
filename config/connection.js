8c1f0cc958d65e755d9ac476b1d375000981a633
Helen Khoda <helen_khoda@yahoo.com>
2019-07-26 15:06:50 -0500

    final

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL){
+  connecttion = mysql.createConnection(process.env.jawsdb_url);
+} else {
+  connection = mysql.createConnection({
+    
+  
     host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
     database: "burger_db"
   });
+};
   
   connection.connect(function(err) {
     if (err) {
